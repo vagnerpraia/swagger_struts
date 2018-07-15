c159e57ff8b0334eefd405091ab5211615c23e6a
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-07-15 06:27:39 -0300

    Ajustando JSON com resultado final

--- swagger_struts/commands/mount.py
+++ swagger_struts/commands/mount.py
@@ -30,48 +30,49 @@ def execute(arguments = {}):
     else:
         if not path.isdir(destination_directory_adjusted):
             destination_file = origin_directory + bar_type + default_filename
-    
-    result_data = {}
+
+    result_data = {name_object_root: {}}
+    list_code = []
     for directory_name, directory_names, filenames in walk(origin_directory):
         for filename in filenames:
             structure_file = path.join(directory_name, filename).split(origin_directory)[1].lstrip('\\/"')
             list_structure_file = structure_file.split(bar_type)
 
-            code = ''
             for name in list_structure_file:
                 list_path = [x for x in list_structure_file if list_structure_file.index(x) < list_structure_file.index(name)]
+                list_path.insert(0, name_object_root)
                 list_path_adjusted = str(list_path).replace(', ', '][')
 
+                code = '{}'
                 if name == filename:
                     object_name = '.'.join(name.split('.')[:-1])
                     type_name = name.replace(object_name, '')
                     path_file = directory_name + bar_type + filename
 
-                    file_content = '{}'
+                    file_content = ''
                     if type_name == '.yaml':
                         yaml_content = read_yaml(path_file)
                         file_content = yaml_to_json(yaml_content)
                     elif type_name == '.json':
-                        file_content = read_file(path_file)
+                        json_content = read_file(path_file)
+                        json_content_adjusted = json_content.replace('\n', '').replace('\t', '')
+                        file_content = json_content_adjusted
 
-                    file_content_adjusted = file_content.replace('true', 'True').replace('false', 'False')
+                    if file_content:
+                        file_content_adjusted = file_content.replace('true', 'True').replace('false', 'False')
 
-                    if (directory_name + bar_type + filename) == origin_file:
-                        object_name = name_object_root
+                        if (directory_name + bar_type + filename) == origin_file:
+                            object_name = name_object_root
 
-                    if list_path:
-                        code += 'result_data' + list_path_adjusted + '.update({\'' + object_name + '\': ' + file_content_adjusted + '})'
-                    else:
-                        code += 'result_data' + '.update({\'' + object_name + '\': ' + file_content_adjusted + '})'
-
-                    eval(code)
-
+                        code = 'result_data' + list_path_adjusted + '.update(' + file_content_adjusted + '),'
                 else:
-                    if list_path:
-                        code += 'result_data' + list_path_adjusted + '.update({\'' + name + '\': {}}),'
-                    else:
-                        code += 'result_data' + '.update({\'' + name + '\': {}}),'
-    
+                    code = 'result_data' + list_path_adjusted + '.update({\'' + name + '\': {}}),'   
+                    
+                if code:
+                    if code not in list_code:
+                        eval(code)                        
+                        list_code.append(code)
+
     result_data_adjusted = str(result_data)\
         .replace('\'', '"')\
         .replace('True', 'true')\
